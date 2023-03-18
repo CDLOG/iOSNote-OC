8fd309049e6e3cc62f4548bef5d729565200e49c
陈乐杰 <1946363509@qq.com>
2023-03-19 02:24:55 +0800

    控制器滑动移除

--- iOSNote-OC/UI/主流框架/page/NTNavigationVC.m
+++ iOSNote-OC/UI/主流框架/page/NTNavigationVC.m
@@ -7,7 +7,7 @@
 
 #import "NTNavigationVC.h"
 
-@interface NTNavigationVC ()<UINavigationControllerDelegate>
+@interface NTNavigationVC ()<UINavigationControllerDelegate,UIGestureRecognizerDelegate>
 /** 系统手势代理 */
 @property (nonatomic, strong) id popGesture;
 
@@ -20,10 +20,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self allScreenMove];
+    
+}
+
+/// 系统滑动移除控制器的方法(只有滑动左边才行)
+-(void)systemMove{
     //保存滑动返回手势代理
     self.popGesture = self.interactivePopGestureRecognizer.delegate;
     // 当是跟控制器,还原代理,如果是非跟控制器,清空代理
     self.delegate = self;
+}
+
+/// 全屏滑动移除控制器
+-(void)allScreenMove{
+    
+    UIScreenEdgePanGestureRecognizer *gest = self.interactivePopGestureRecognizer;
+    
+
+    id target = self.interactivePopGestureRecognizer.delegate;
+//    // 2.自己添加手势
+//    // 禁止系统的手势
+    self.interactivePopGestureRecognizer.enabled = NO;
+    
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:target action:@selector(handleNavigationTransition:)];
+    
+    [self.view addGestureRecognizer:pan];
+
+    pan.delegate = self;
+}
+#pragma mark - UIGestureRecognizerDelegate
+// 当开始滑动的就会调用 如果返回YES ,可以滑动 返回NO,禁止手势
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer{
+    // 当是跟控制器不让移除(禁止), 非根控制器,允许移除控制
+    BOOL open = self.viewControllers.count > 1;
+    
+    return open;
 }
 
 #pragma mark - UINavigationControllerDelegate
